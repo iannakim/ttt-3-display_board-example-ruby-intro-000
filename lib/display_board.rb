8436be285677afa4b1e4c46f2ab615473a908a50
Hyun Ji  Kim <dear.hyunji@gmail.com>
2020-03-08 17:53:44 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 def display_board
   puts "A Tic Tac Toe Board"
   puts "   |   |   "
-  puts "___________"
+  puts "-----------"
   puts "   |   |   "
-  puts "___________"
+  puts "-----------"
   puts "   |   |   "
 end
